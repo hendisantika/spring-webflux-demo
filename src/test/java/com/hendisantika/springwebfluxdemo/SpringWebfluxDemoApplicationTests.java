1c560da57a3618bf1f07e380dbdd8d3319c0d744
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-25 06:09:38 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springwebfluxdemo/SpringWebfluxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springwebfluxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
